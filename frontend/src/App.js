dbca38cfb252f4d37a542dacdd2e3c39c6640292
Maleesha Pathirana <maleeshapathirana1@gmail.com>
2024-08-09 22:49:55 +0530

    test

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,8 @@
-import logo from './logo.svg';
+
 import './App.css';
 import {BrowserRouter,Routes,Route} from 'react-router-dom';
 import Login from '../src/Pages/Login';
+import Dashboard from './Pages/Super Admin/Dashboard';
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
       
       <Routes>
         <Route path='/' element={<Login/>} />
+        
 
       </Routes>
 
