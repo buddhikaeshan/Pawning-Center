9ef325ce59f05ea8d3ce18dbedecd1e55f2bb005
buddhikaeshan <buddhikaeshan12@gmail.com>
2024-08-28 23:24:55 +0530

    interest

--- frontend/src/Pages/Admin/InterestAdmin.jsx
+++ frontend/src/Pages/Admin/InterestAdmin.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import Sidebar from '../../components/Sidebar';
+import Sidebar from '../../components/SidebarAdmin';
 import './Interest.css'
 import { useState } from 'react';
 
